8341e8fa0dad36bc6e56b53a561dbc9e5edb8b6f
paulschick <paulschick19@gmail.com>
2024-01-01 15:00:05 -0500

    implement config package for cli

--- cmd/disclosurecli/main.go
+++ cmd/disclosurecli/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/paulschick/disclosureupdater/util"
+	"github.com/paulschick/disclosureupdater/config"
 	"github.com/spf13/viper"
 	"github.com/urfave/cli/v2"
 	"log"
@@ -13,19 +13,6 @@ import (
 // https://github.com/spf13/viper
 func main() {
 	var err error
-
-	viper.SetConfigFile("config.yaml")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-
-	// TODO - this is going to be the default configuration file and app directory
-	overridesDir := path.Join(os.Getenv("HOME"), ".disclosurecli")
-
-	err = util.TryCreateDirectories(overridesDir)
-	if err != nil {
-		log.Fatalf("Error creating overrides directory: %s", err)
-	}
-	overridesPath := path.Join(overridesDir, "overrides.yaml")
 
 	cli.VersionFlag = &cli.BoolFlag{
 		Name:    "version",
@@ -90,7 +77,6 @@ func main() {
 							viper.Set("default.s3.s3Hostname", s3Hostname)
 							viper.Set("default.s3.s3ApiKey", s3ApiKey)
 							viper.Set("default.s3.s3SecretKey", s3SecretKey)
-							log.Printf("Overrides path: %s\n", overridesPath)
 							return viper.WriteConfig()
 						},
 						Flags: []cli.Flag{
@@ -131,57 +117,6 @@ func main() {
 								EnvVars: []string{"S3_SECRET_KEY"},
 							},
 						},
-						Subcommands: []*cli.Command{
-							{
-								Name:    "override",
-								Aliases: []string{"o"},
-								Usage:   "Override configuration file location and name",
-								UsageText: "Create a new file path and/or file name for " +
-									"the configuration file",
-								Action: func(cCtx *cli.Context) error {
-									log.Println("Overriding configuration file location and name")
-									newPath := cCtx.String("config-directory")
-									log.Printf("New path: %s\n", newPath)
-									newFile := cCtx.String("config-file")
-									log.Printf("New file: %s\n", newFile)
-
-									if newPath != "" || newFile != "" {
-										log.Println("Overriding configuration file location and name")
-										// Create/Update overrides file
-										viper.SetConfigFile(overridesPath)
-										viper.SetConfigType("yaml")
-										viper.Set("configDirectory", newPath)
-										viper.Set("configFile", newFile)
-										err = viper.WriteConfig()
-										if err != nil {
-											log.Fatalf("Error writing overrides file: %s", err)
-										}
-
-										// Then write the new configuration file
-										viper.SetConfigFile(path.Join(newPath, newFile))
-										viper.SetConfigType("yaml")
-										err = viper.WriteConfig()
-										if err != nil {
-											log.Fatalf("Error writing new configuration file: %s", err)
-										}
-									}
-									return nil
-								},
-								Flags: []cli.Flag{
-									&cli.StringFlag{
-										Name:    "config-directory",
-										Usage:   "Configuration file path - Provide an existing directory",
-										Aliases: []string{"d"},
-									},
-									&cli.StringFlag{
-										Name: "config-file",
-										Usage: "Configuration file name - Provide a new file name with " +
-											".yaml extension.\nMust provide a yaml configuration file.",
-										Aliases: []string{"f"},
-									},
-								},
-							},
-						},
 					},
 				},
 				Action: func(cCtx *cli.Context) error {
@@ -202,92 +137,22 @@ func runApp(app *cli.App) error {
 	return app.Run(os.Args)
 }
 
-type Directories struct {
-	BaseDir string
-	DataDir string
-	S3Dir   string
-}
-
-func NewDirectories() *Directories {
-	baseDir := path.Join(os.Getenv("HOME"), ".disclosurecli")
-	dataDir := path.Join(baseDir, "data")
-	s3Dir := path.Join(baseDir, "s3")
-	return &Directories{
-		BaseDir: baseDir,
-		DataDir: dataDir,
-		S3Dir:   s3Dir,
-	}
-}
-
-func (d *Directories) CreateDirectories() error {
-	err := util.TryCreateDirectories(d.DataDir)
-	if err != nil {
-		return err
-	}
-	err = util.TryCreateDirectories(d.S3Dir)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (d *Directories) CreateExampleConfigFile() error {
-	defaultProfile := NewDefaultProfile(d)
-	configYamlFile := defaultProfile.YamlFile
-	viper.SetConfigFile(configYamlFile)
-	viper.SetConfigType("yaml")
-	viper.Set("default.dataFolder", defaultProfile.DataFolder)
-	viper.Set("default.s3.s3Bucket", defaultProfile.S3.S3Bucket)
-	viper.Set("default.s3.s3Region", defaultProfile.S3.S3Region)
-	viper.Set("default.s3.s3Hostname", defaultProfile.S3.S3Hostname)
-	viper.Set("default.s3.s3ApiKey", defaultProfile.S3.S3ApiKey)
-	viper.Set("default.s3.s3SecretKey", defaultProfile.S3.S3SecretKey)
-
-	err := viper.WriteConfig()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-type S3Profile struct {
-	S3Bucket    string
-	S3Region    string
-	S3Hostname  string
-	S3ApiKey    string
-	S3SecretKey string
-}
-
-type Profile struct {
-	Name       string
-	YamlFile   string
-	DataFolder string
-	S3         *S3Profile
-}
-
-func NewDefaultProfile(directories *Directories) *Profile {
-	return &Profile{
-		Name:       "default",
-		YamlFile:   path.Join(directories.BaseDir, "config.yaml"),
-		DataFolder: directories.DataDir,
-		S3: &S3Profile{
-			S3Bucket:    "<bucket>",
-			S3Region:    "<region>",
-			S3Hostname:  "<hostname>",
-			S3ApiKey:    "<api-key>",
-			S3SecretKey: "<secret-key>",
-		},
-	}
-}
-
 func initialize() error {
 	var err error
-	directories := NewDirectories()
-	err = directories.CreateDirectories()
+	profile := "default"
+	baseDir := path.Join(os.Getenv("HOME"), ".disclosurecli")
+	commonDirs := config.NewCommonDirs(baseDir)
+	err = commonDirs.CreateDirectories()
 	if err != nil {
 		return err
 	}
-	err = directories.CreateExampleConfigFile()
+	s3Default := config.S3DefaultProfile{
+		S3Bucket:   "<bucket>",
+		S3Region:   "<region>",
+		S3Hostname: "<hostname>",
+	}
+	v := config.BuildViper(profile, commonDirs, &s3Default)
+	err = v.WriteConfig()
 	if err != nil {
 		return err
 	}
