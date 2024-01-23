ce448dd26cc111159a5fdc34c5ee1ef5d2cf3985
paulschick <paulschick19@gmail.com>
2024-01-23 08:25:30 -0500

    Implement v2 pdf to png conversion
    * Changes:
    *    V2 PDF to PNG conversion improves performance by
         updating the handling of goroutines.
    *    I/O operations and conversion processes run
         asynchronously, and the number of goroutines
         is limited by a value dependent on the available
         CPU cores.

---
+++ cmds/pdfConversionV2.go
@@ -0,0 +1,139 @@
+package cmds
+
+import (
+	"fmt"
+	"github.com/gen2brain/go-fitz"
+	"image"
+	"image/png"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+type ConversionResult struct {
+	Image     image.Image
+	ImageName string
+	ImageDir  string
+}
+
+type PdfConverterV2 struct {
+	PdfPath      string
+	BaseFileName string
+	ImageDir     string
+}
+
+// NewPdfConverterV2
+// TODO - use a flat file structure instead of the nested structure
+// images will be organized by {name}-{page no}.png, so they'll maintain order
+func NewPdfConverterV2(pdfPath, imageDir string) *PdfConverterV2 {
+	return &PdfConverterV2{
+		PdfPath:      pdfPath,
+		ImageDir:     imageDir,
+		BaseFileName: filepath.Base(strings.Split(pdfPath, ".pdf")[0]),
+	}
+}
+
+func (p *PdfConverterV2) GetImageName(pageNumber int, extension string) string {
+	return fmt.Sprintf("%s-%d%s", p.BaseFileName, pageNumber, extension)
+}
+
+func (p *PdfConverterV2) ConvertPagesToImages(extension string) ([]ConversionResult, error) {
+	var results []ConversionResult
+	var err error
+	var doc *fitz.Document
+
+	doc, err = fitz.New(p.PdfPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(err error) {
+		err = doc.Close()
+	}(err)
+
+	for n := 0; n < doc.NumPage(); n++ {
+		var img image.Image
+		img, err = doc.Image(n)
+		if err != nil {
+			return nil, err
+		}
+		imageName := p.GetImageName(n, extension)
+		results = append(results, ConversionResult{
+			Image:     img,
+			ImageName: imageName,
+			ImageDir:  p.ImageDir,
+		})
+	}
+
+	return results, err
+}
+
+func WriteImage(result ConversionResult) error {
+	var err error
+	var f *os.File
+
+	f, err = os.Create(filepath.Join(result.ImageDir, result.ImageName))
+
+	if err != nil {
+		_ = f.Close()
+		return err
+	}
+
+	err = png.Encode(f, result.Image)
+
+	if err != nil {
+		_ = f.Close()
+		return err
+	}
+
+	return f.Close()
+}
+
+func BatchPdfToPng(pdfDir, imageDir string) error {
+	pdfs, err := os.ReadDir(pdfDir)
+	if err != nil {
+		return err
+	}
+	pdfs = pdfs[:100]
+
+	numCpu := runtime.NumCPU()
+	maxJobs := numCpu * 2
+	waitChan := make(chan struct{}, maxJobs)
+	var wg sync.WaitGroup
+	var writeWg sync.WaitGroup
+
+	for _, pdf := range pdfs {
+		waitChan <- struct{}{}
+		wg.Add(1)
+		go func(pdf os.DirEntry) {
+			defer wg.Done()
+
+			pdfConverter := NewPdfConverterV2(filepath.Join(pdfDir, pdf.Name()), imageDir)
+			results, err := pdfConverter.ConvertPagesToImages(".png")
+			if err != nil {
+				// TODO - handle error
+				fmt.Println(err)
+			}
+
+			for _, result := range results {
+				writeWg.Add(1)
+				go func(res ConversionResult) {
+					defer writeWg.Done()
+					if err = WriteImage(res); err != nil {
+						// TODO - handle error
+						fmt.Printf("error writing image %s\n", res.ImageName)
+						fmt.Println(err)
+					}
+				}(result)
+			}
+
+			<-waitChan
+		}(pdf)
+	}
+
+	wg.Wait()
+	writeWg.Wait()
+	return nil
+}
